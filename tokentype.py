2d8e827c5e9b69df7c48e5fd16c9d2c42d798a0b
Ak4ai <129908980+Ak4ai@users.noreply.github.com>
2026-03-19 21:28:53 -0300

    fix: alinha tokentype.py com projeto de base - CABOU, CERTIN/ERADIN, TREM_DI_NUMERU, comentarios

--- tokentype.py
+++ tokentype.py
@@ -25,32 +25,54 @@ class TokenType(Enum):
     STRING_LITERAL = "STRING_LITERAL"
     CHAR_LITERAL = "CHAR_LITERAL"
 
-    # Palavras reservadas
-    BORA_CUMPADE = "BORA_CUMPADE"
-    SIMBORA = "SIMBORA"
-    CABO = "CABO"
-    UAI_SE = "UAI_SE"
-    UAI_SENAO = "UAI_SENAO"
-    ENQUANTO_TIVER_TREM = "ENQUANTO_TIVER_TREM"
+    # Condicionais
+    C_TO_PENSANU = "C_TO_PENSANU"
+    C_NAO = "C_NAO"
+    C_NAO_C_TO_PENSANU = "C_NAO_C_TO_PENSANU"
+
+    # Laços
     RODA_ESSE_TREM = "RODA_ESSE_TREM"
+    ENQUANTO_TIVER_TREM = "ENQUANTO_TIVER_TREM"
+
+    # Switch/Case
     DEPENDENU = "DEPENDENU"
     DU_CASU = "DU_CASU"
+
+    # Fluxo
     TA_BAO = "TA_BAO"
     PARA_O_TREM = "PARA_O_TREM"
     TOCA_O_TREM = "TOCA_O_TREM"
+
+    # Main
+    BORA_CUMPADE = "BORA_CUMPADE"
+
+    # Escopo
+    SIMBORA = "SIMBORA"
+    CABOU = "CABOU"
+
+    # Entrada/Saída
     XOVE = "XOVE"
     OIA_PROCE_VE = "OIA_PROCE_VE"
 
+    # Condicionais legado
+    UAI_SE = "UAI_SE"
+    UAI_SENAO = "UAI_SENAO"
+
     # Nomes de tipos
-    TREM_DI_NUMERO = "TREM_DI_NUMERO"
+    TREM_DI_NUMERU = "TREM_DI_NUMERU"
     TREM_CUM_VIRGULA = "TREM_CUM_VIRGULA"
     TREM_DISCRITA = "TREM_DISCRITA"
     TREM_DISCOLHE = "TREM_DISCOLHE"
     TROSSO = "TROSSO"
 
     # Literais booleanos
-    TRUE = "TRUE"
-    FALSE = "FALSE"
+    CERTIN = "CERTIN"
+    ERADIN = "ERADIN"
+
+    # Comentários
+    COMMENT_LINE = "COMMENT_LINE"
+    COMMENT_START = "COMMENT_START"
+    COMMENT_END = "COMMENT_END"
 
     # Operadores por palavra
     FICA_ASSIM_ENTAO = "FICA_ASSIM_ENTAO"
@@ -82,25 +104,28 @@ class TokenType(Enum):
 
 
 KEYWORD_TOKENS = {
-    "bora_cumpade": TokenType.BORA_CUMPADE,
-    "simbora": TokenType.SIMBORA,
-    "cabo": TokenType.CABO,
-    "uai_se": TokenType.UAI_SE,
-    "uai_senao": TokenType.UAI_SENAO,
-    "enquanto_tiver_trem": TokenType.ENQUANTO_TIVER_TREM,
+    "c_to_pensanu": TokenType.C_TO_PENSANU,
+    "c_nao": TokenType.C_NAO,
+    "c_nao_c_to_pensanu": TokenType.C_NAO_C_TO_PENSANU,
     "roda_esse_trem": TokenType.RODA_ESSE_TREM,
+    "enquanto_tiver_trem": TokenType.ENQUANTO_TIVER_TREM,
     "dependenu": TokenType.DEPENDENU,
     "du_casu": TokenType.DU_CASU,
     "ta_bao": TokenType.TA_BAO,
     "para_o_trem": TokenType.PARA_O_TREM,
     "toca_o_trem": TokenType.TOCA_O_TREM,
+    "bora_cumpade": TokenType.BORA_CUMPADE,
+    "simbora": TokenType.SIMBORA,
+    "cabou": TokenType.CABOU,
     "xove": TokenType.XOVE,
     "oia_proce_ve": TokenType.OIA_PROCE_VE,
+    "uai_se": TokenType.UAI_SE,
+    "uai_senao": TokenType.UAI_SENAO,
 }
 
 
 TYPE_TOKENS = {
-    "trem_di_numero": TokenType.TREM_DI_NUMERO,
+    "trem_di_numeru": TokenType.TREM_DI_NUMERU,
     "trem_cum_virgula": TokenType.TREM_CUM_VIRGULA,
     "trem_discrita": TokenType.TREM_DISCRITA,
     "trem_discolhe": TokenType.TREM_DISCOLHE,
@@ -109,8 +134,14 @@ TYPE_TOKENS = {
 
 
 BOOLEAN_TOKENS = {
-    "eh": TokenType.TRUE,
-    "num_eh": TokenType.FALSE,
+    "certin": TokenType.CERTIN,
+    "eradin": TokenType.ERADIN,
+}
+
+
+COMMENT_TOKENS = {
+    "causo": TokenType.COMMENT_START,
+    "fim_do_causo": TokenType.COMMENT_END,
 }
 
 
@@ -136,6 +167,7 @@ ALL_WORD_TOKENS = {
     **KEYWORD_TOKENS,
     **TYPE_TOKENS,
     **BOOLEAN_TOKENS,
+    **COMMENT_TOKENS,
     **WORD_OPERATOR_TOKENS,
     **WORD_DELIMITER_TOKENS,
 }
